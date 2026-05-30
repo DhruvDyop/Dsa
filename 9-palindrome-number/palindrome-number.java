b0b9dee4e99a38a1ce816bd468238456fc60e5ae
Dhruv Yadav <yadav.dhruv11@gmail.com>
2026-05-30 21:26:41 +0530

    Time: 5 ms (78.45%) | Memory: 45.6 MB (92.95%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        long x1 = x, x2 = 0;
+        while (x > 0) {
+            x2 = x2 * 10 + x % 10;
+            x /= 10;
+        }
+        return x1 == x2; 
+    }
+}
